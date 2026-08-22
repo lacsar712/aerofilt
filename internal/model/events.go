f08fcd2cadfa7dbef18135c13e208cd03437c8d4
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-23 00:51:02 +0800

    init: aerofilt

---
+++ internal/model/events.go
@@ -0,0 +1,13 @@
+package model
+
+const (
+	EventHeadIngest    = "head_ingest"
+	EventBackwashStart = "backwash_start"
+	EventBackwashDone  = "backwash_done"
+	EventWashTransit   = "wash_transit"
+	EventBlowerCommand = "blower_command"
+	EventInterlockDeny = "interlock_deny"
+	EventEStop         = "estop"
+	EventAlarmRaise    = "alarm_raise"
+	EventAlarmClear    = "alarm_clear"
+)
